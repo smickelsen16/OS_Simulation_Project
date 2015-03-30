d5190f82af2f09e8bc6df0dd60abb79e584b59c7
Sarah M <smickelsen16@my.whitworth.edu>
2015-03-30 10:06:51 -0700

    start

---
+++ OS_Simulation_Project/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Simulation_Project
+{
+
+    
+
+    /// <summary>
+    /// holds a dictionary mapping of PID's and PCB's
+    /// the simulation will pull processes from here to run 
+    /// </summary>
+    class ProcessTable
+    {
+        ProcessTable(Dictionary<int, PCB> d) { this.dictionary = d; }
+        Dictionary<int, PCB> dictionary;            // dictionary of PID's (KEY, int) and PCB's (VALUE)
+    }
+
+    /// <summary>
+    /// holds all stat info for each individual process
+    /// </summary>
+    class PCB
+    {
+        public PCB(double expSerT, bool state, double arrive) {
+            this.execution = 0; 
+            this.expectedServiceTime = expSerT;
+            this.remainingServiceTime = expSerT;
+            this.processState = state; 
+            this.response = -1; 
+            this.turnaround = 0;
+            this.wait = 0; 
+            this.arrivalTime = arrive;
+            this.name = "Process " + arrivalTime.ToString();
+        }
+
+        public double execution;                           // time spent in execution thus far
+        public double expectedServiceTime;                 // service time required by process
+        public double remainingServiceTime;                // keeps track of remaining service time as process goes through queue
+        public bool processState;                          // true = ready, false = waiting
+        public double response;                            // time from request submission to first response
+        public double turnaround;                          // time from submission to completion of process
+        public double wait;                                // time process spends in ready queues
+        public double arrivalTime;                         // time process arrives in queue
+        public string name;
+
+        public override string ToString()
+        {
+            return name + "'s Stats\n" +
+                "Expected Service Time: " + expectedServiceTime.ToString() +
+                "\nArrival Time: " + arrivalTime.ToString() +
+                "\nProcess State: " + processState.ToString() +
+                "\nResponse: " + response.ToString() +
+                "\nTurnaround: " + turnaround.ToString() +
+                "\nWait: " + wait.ToString();
+        }
+
+    }
+
+    /// <summary>
+    /// where all the magic will happen
+    /// </summary>
+    class Simulation
+    {
+        //public ProcessTable table;                       // list of all processes & associated info & ID's
+
+
+        static void Main()
+        {
+            double throughput;                          // amount of processes completed in a given time
+            double CPU_utilization;                     // % time CPU is executing
+
+            Dictionary<int, PCB> readyQ = new Dictionary<int, PCB>();
+            readyQ.Add(0, new PCB(6, true, 0));
+            readyQ.Add(1, new PCB(8, true, 4));
+            readyQ.Add(2, new PCB(4, true, 7));
+            readyQ.Add(3, new PCB(4, true, 8));
+            readyQ.Add(4, new PCB(2, true, 10));
+            readyQ.Add(5, new PCB(7, true, 15));
+            readyQ.Add(6, new PCB(5, true, 17));
+
+            UniprocessorAlgorithms uniSim = new UniprocessorAlgorithms();
+
+            uniSim.First_Come_First_Served(readyQ);                     // FCFS works correctly!!
+            //throughput = readyQ.Count() / uniSim.systemTime;            // calculate throughput (# procs/system time)
+            //Console.WriteLine("Throughput for FCFS: " + throughput.ToString() + " processes/system time\n");
+
+            uniSim.Round_Robin(4,readyQ);                                   // issues when a process arrives after quantum is over....
+            //throughput = readyQ.Count() / uniSim.systemTime;            // calculate throughput (# procs/system time)
+            //Console.WriteLine("Throughput for Round Robin: " + throughput.ToString() + " processes/system time\n");
+        }
+    }
+}
---
+++ OS_Simulation_Project/UniprocessorAlgorithms.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Simulation_Project
+{
+    class UniprocessorAlgorithms
+    {
+        public double systemTime = 0;          // keeps track of current system time
+
+        /// <summary>
+        /// Preemptive (?)
+        /// Runs each process for the specified time quantum and then switches to the next arrived process
+        /// </summary>
+        /// <param name="quantum"> time allocated to each process per RR cycle </param>
+        /// <param name="ReadyQueue"> list of processes to be run </param>
+        public void Round_Robin(int quantum, Dictionary<int, PCB> ReadyQueue)
+        {
+            PCB currentProc;
+            Dictionary<int, PCB> CompletedProcs = new Dictionary<int, PCB>();
+            int counter = 0;
+            do
+            {
+                // find the earliest arriving process 
+                if (counter < ReadyQueue.Count())
+                    currentProc = ReadyQueue.ElementAt(counter).Value;
+                else
+                    currentProc = ReadyQueue.First().Value;                          // if the counter is bigger than the Queue, just get the first value
+                    
+                // set the processes response time to the systemTime - arrivalTime
+                if (currentProc.response == -1)
+                {
+                    if (currentProc.arrivalTime <= systemTime)                      // make sure a new process has arrived
+                        currentProc.response = systemTime - currentProc.arrivalTime;
+                    else
+                        // need to make sure this counts as CPU idle time when the system has to wait for a process to arrive... or does it run previous process again?
+                        systemTime += (currentProc.arrivalTime - systemTime);       // if no new process, update the system time to show that time has passed where the CPU was idle
+                }
+
+                // run that process for the quantum 
+                if (currentProc.remainingServiceTime >= quantum ) {                 // check to make sure quantum isn't bigger than remaining time
+                    currentProc.remainingServiceTime -= quantum;                    // subtract quantum from remainingServiceTime
+                    systemTime += quantum;                                          //  update system time 
+                }
+                else { 
+                    systemTime += currentProc.remainingServiceTime;     // udate system time
+                    currentProc.remainingServiceTime = 0;                           // zero out remainingServiceTime
+                }
+                
+
+                // check to see if process has finished
+                if (currentProc.remainingServiceTime == 0)
+                {
+                    currentProc.execution = systemTime;                                         // set execution time to current system time
+                    currentProc.turnaround = systemTime - currentProc.arrivalTime;              // set turnaround time to systemTime - arrivalTime
+                    currentProc.wait = currentProc.turnaround - currentProc.expectedServiceTime;                                           
+                    // write stat information to file and remove process from ReadyQueue & add to CompletedProcs
+                    if (counter < ReadyQueue.Count())             // check if counter > # procs left
+                    {
+                        CompletedProcs.Add(ReadyQueue.ElementAt(counter).Key, ReadyQueue.ElementAt(counter).Value);         // add to completed procs
+                        ReadyQueue.Remove(ReadyQueue.ElementAt(counter).Key);                                               // remove from readyQueue
+                        counter--;                                                                                          // if a proc is removed, decrement the counter
+                    }
+                    else
+                    {
+                        CompletedProcs.Add(ReadyQueue.First().Key, ReadyQueue.First().Value);                    // add to completed procs
+                        ReadyQueue.Remove(ReadyQueue.First().Key);                                               // remove from readyQueue
+                        counter--;                                                                               // if a proc is removed, decrement the counter
+                    }
+                    
+                }
+
+                counter++;                                                          // update the counter variable to get the next key/value pair
+
+            } while (ReadyQueue.Count() != 0);
+
+            for (int i = 0; i < CompletedProcs.Count(); i++)
+                Console.WriteLine(CompletedProcs.ElementAt(i).Value.ToString() + "\n");
+
+            Console.WriteLine("Round Robin Complete\n");
+        }
+
+        /// <summary>
+        /// Non-preemptive
+        /// Runs each process to completion based on the time they arrive
+        /// </summary>
+        /// <param name="ReadyQueue"> list of processes to be run </param>
+        public void First_Come_First_Served(Dictionary<int, PCB> ReadyQueue)
+        {
+            PCB currentProc;
+            Dictionary<int, PCB> CompletedProcs = new Dictionary<int, PCB>();
+            
+            for (int i = 0; i < ReadyQueue.Count(); i++)
+            {
+                // find the earliest arriving process
+                currentProc = ReadyQueue.ElementAt(i).Value;
+
+                // set the processes response time to the systemTime - arrivalTime
+                currentProc.response = systemTime - currentProc.arrivalTime;
+
+                // run the process to completion
+                systemTime += currentProc.expectedServiceTime;                      // update system time to account for running the program 
+                currentProc.remainingServiceTime = 0;                               // process has completed
+                currentProc.execution = systemTime - currentProc.response;          // update currentProc execution time to systemTime - currentProc.response
+                currentProc.turnaround = systemTime - currentProc.arrivalTime;      // set turnaround time to systemTime - arrivalTime
+                currentProc.wait = currentProc.response;                            // how to calculate wait time for  each process?
+                // write stat information to file and remove process from ReadyQueue & add to CompletedProcs
+
+                //Console.WriteLine(currentProc.name + " has completed.");
+
+            }
+
+            for (int i = 0; i < ReadyQueue.Count(); i++)
+                Console.WriteLine(ReadyQueue.ElementAt(i).Value.ToString() + "\n");
+
+            Console.WriteLine("FCFS Complete");
+                
+        }
+
+        /// <summary>
+        /// Preemptive
+        /// Runs & completes shortest processes first
+        /// Will preempt current process if a shorter process arrives during execution
+        /// </summary>
+        /// <param name="ReadyQueue"> list of processes to be run </param>
+        public void Shortest_Process_Next(Dictionary<int, PCB> ReadyQueue)
+        {
+            PCB currentProc;
+            Dictionary<int, PCB> CompletedProcs = new Dictionary<int, PCB>();
+            int counter = 0;
+            do
+            {
+                currentProc = ReadyQueue.ElementAt(counter).Value;
+
+                // start current process
+                currentProc.response = systemTime - currentProc.arrivalTime;              // set response time 
+
+                // check every clock cycle to see if a shorter process arrives
+                // if yes, stop current proc, move to shorter proc & repeat
+                // if no, continue
+            } while (ReadyQueue.Count() != 0);
+        }
+
+        /// <summary>
+        /// Preemptive
+        /// Completes the process with the shortest remaining time
+        /// Will interrupt the current process if another process has a shorter remaining time 
+        /// </summary>
+        /// <param name="processes"> list of processes to be run </param>
+        public void Shortest_Remaining_Time(Dictionary<int, PCB> ReadyQueue)
+        {
+            PCB currentProc;
+            Dictionary<int, PCB> CompletedProcs = new Dictionary<int, PCB>();
+
+            // start current process
+            // check every clock cycle to see if a process has shorter remaining time
+            // if yes, stop current proc, move to shorter proc & repeat
+            // if no, continue
+        }
+    }
+
+    
+}
