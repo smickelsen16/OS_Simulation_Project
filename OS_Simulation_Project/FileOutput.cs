8a13e2cd41d796e8e9aabc45fb6db76304909845
TylerHarding <tharding732@gmail.com>
2015-04-07 16:31:12 -0700

    Finished creating Excel file
    I think I got the code working that will be used to create an excel
    file. Now just need to work on getting values from the algorithms and
    writing them to the excel sheet with it having proper formatting.

--- OS_Simulation_Project/FileOutput.cs
+++ OS_Simulation_Project/FileOutput.cs
@@ -24,6 +24,34 @@ namespace OS_Simulation_Project
 
             xlWorkBook = xlApp.Workbooks.Add(misValue);
             xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            xlWorkSheet.Cells[1, 1] = "Sheet1 content";
+
+            xlWorkBook.SaveAs("C:\Users\James Bond\Documents\GitHub\OS_Simulation_Project\ExcelSheet.xls",Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkBook);
+            releaseObject(xlWorkSheet);
+            releaseObject(xlApp);
+            
+        }
+
+        private void releaseObject(object obj) 
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }        
+            catch(Exception ex)
+            {
+                obj = null;
+                Console.WriteLine("Exception Occured while releasing object");
+            }
+            finally
+            {
+                GC.Collect();
+            }
         }
         
         
