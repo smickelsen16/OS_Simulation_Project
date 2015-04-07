a385e3e1bf291cb5c5baf0feee59fc0fba38c03c
TylerHarding <tharding732@gmail.com>
2015-04-07 16:10:36 -0700

    Starting work on Excel File output
    Stuff

---
+++ OS_Simulation_Project/FileOutput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OS_Simulation_Project
+{
+    class FileOutput
+    {
+        public void doStuff()
+        {
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                Console.WriteLine("Excel is not properly installed!");
+            }
+
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+        }
+        
+        
+    }
+}
